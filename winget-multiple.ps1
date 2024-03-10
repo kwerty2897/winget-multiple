8ec2a8807cddfef541aaa729aa6c67d86d7770dd
kwerty2897 <102196327+kwerty2897@users.noreply.github.com>
2024-03-11 00:06:10 +0300

    Add files via upload

---
+++ winget-multiple.ps1
@@ -0,0 +1,22 @@
+#Requires -RunAsAdministrator
+Write-Host "Checking WinGet..."
+
+Try{
+    # Check if winget is already installed
+    $er = (invoke-expression "winget -v") 2>&1
+    if ($lastexitcode) {throw $er}
+    Write-Host "WinGet is already installed."
+}
+Catch{
+    Write-Host -Foreground Green 'Not found, trying install it'
+    Start-Process 'ms-windows-store://pdp?productid=9NBLGGH4NNS1'
+    Write-Host -Foreground Red 'If you dont have Store try wsreset.exe from admin in terminal shell'
+    Read-Host -Prompt "Install/update App Installer in MS Store window and press any key to continue"    
+}
+Finally {
+    Write-Host -Foreground Green "Installing WinGet packages"
+    Get-Content .\packages.txt | ForEach-Object {
+    $appName = $_
+    iex ("winget install $appName --silent --accept-source-agreements --accept-package-agreements")
+    }
+}
